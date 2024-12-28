d711c691e698fa13cf2c557163468e03ef2bdf16
Anthony_D <anthonydiaz1597@gmail.com>
2024-12-28 14:41:54 +0800

    login update-

--- app/src/main/java/com/example/mobilepayroll/AdminDashboard.java
+++ app/src/main/java/com/example/mobilepayroll/AdminDashboard.java
@@ -1,25 +1,53 @@
 package com.example.mobilepayroll;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class AdminDashboard extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        db = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        userId = auth.getCurrentUser().getUid();
+
+        Button gotoProfilePage = findViewById(R.id.profile_page);
+        TextView showAdmin = findViewById(R.id.show_admin);
+
+        // Fetch data from Firestore and update the UI
+        DocumentReference documentReference = db.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    showAdmin.setText(documentSnapshot.getString("fullName"));
+                }
+            }
+        });
+
+        // Set up button click listener
+        gotoProfilePage.setOnClickListener(v -> {
+            Intent GoToProfilePage = new Intent(AdminDashboard.this, Profilepage_function.class);
+            startActivity(GoToProfilePage);
+            finish();
         });
     }
 }
-
